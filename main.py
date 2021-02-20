90f52b8d3b41fb8367b8e8241e1fe1d80e61908b
SUPERustam <498rustam@gmail.com>
2021-02-20 20:06:43 +0300

    add shortcuts

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 
 import requests
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QShortcut
+from PyQt5.QtWidgets import QApplication, QWidget, QShortcut
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QPixmap, QKeySequence
 
-from req_maps import get_map, MyBadRequest
+from req_maps import get_map, MyBadRequest  # MyBadRequest is important
 from config import API_KEY  # api key doesn't work
 
 
@@ -76,37 +76,53 @@ class Front_Widget(QWidget, Ui_Form):
         }
 
         # hot keys
-        self.go_button = QShortcut(QKeySequence("Return"), self)
+        self.go_button = QShortcut(QKeySequence("return"), self)
         self.resolution_up = QShortcut(QKeySequence("PgUp"), self)
         self.resolution_down = QShortcut(QKeySequence("PgDown"), self)
         self.map_left = QShortcut(QKeySequence('Left'), self)
-        # self.map_right = QShortcut(QKeySequence("Right"), self)
-        # self.map_down = QShortcut(QKeySequence("Down"), self)
-        # self.map_up = QShortcut(QKeySequence("Up"), self)
+        self.map_right = QShortcut(QKeySequence("Right"), self)
+        self.map_down = QShortcut(QKeySequence("Down"), self)
+        self.map_up = QShortcut(QKeySequence("Up"), self)
         self.resolution_up.activated.connect(self.fresolution_up)
         self.resolution_down.activated.connect(self.fresolution_down)
         self.go_button.activated.connect(self.shower)
         self.map_left.activated.connect(self.fmap_left)
-        # self.map_right.activated.connect(self.fmap_right)
-        # self.map_down.activated.connect(self.fmap_down)
-        # self.map_up.activated.connect(self.fmap_up)
+        self.map_right.activated.connect(self.fmap_right)
+        self.map_down.activated.connect(self.fmap_down)
+        self.map_up.activated.connect(self.fmap_up)
 
     def fresolution_up(self):
         if self.params_maps["scale"] < 4.0:
             self.params_maps["scale"] += 0.1
-            pass
 
     def fresolution_down(self):
         if self.params_maps["scale"] >= 1.1:
             self.params_maps["scale"] -= 0.1
             self.show_map()
 
-    def fmap_left(self):
+    def fmap_up(self):
         temp_ll = self.params_maps['ll'].split(',')
         temp_ll[1] = str(float(temp_ll[1]) + 0.0005)
         self.params_maps['ll'] = ','.join(temp_ll)
         self.show_map()
-        print('left', self.params_maps)
+
+    def fmap_left(self):
+        temp_ll = self.params_maps['ll'].split(',')
+        temp_ll[0] = str(float(temp_ll[0]) - 0.0005)
+        self.params_maps['ll'] = ','.join(temp_ll)
+        self.show_map()
+
+    def fmap_right(self):
+        temp_ll = self.params_maps['ll'].split(',')
+        temp_ll[0] = str(float(temp_ll[0]) + 0.0005)
+        self.params_maps['ll'] = ','.join(temp_ll)
+        self.show_map()
+
+    def fmap_down(self):
+        temp_ll = self.params_maps['ll'].split(',')
+        temp_ll[1] = str(float(temp_ll[1]) - 0.0005)
+        self.params_maps['ll'] = ','.join(temp_ll)
+        self.show_map()
 
     def shower(self):
         self.params_maps['ll'] = self.cord = self.lineEdit_3.text().replace(' ', '').replace('%2C', ',')
